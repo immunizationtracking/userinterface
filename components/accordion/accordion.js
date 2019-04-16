9110c074625627acf33475f8922660ba98ecc477
HitzeD <d-hitze@comcast.net>
2019-04-16 14:47:05 -0500

    finished accordian JS file

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -2,13 +2,18 @@ class Accordion {
     constructor(member){
         this.member = member;
         this.tab = this.member.querySelector('.member-top');
+        this.img = this.tab.querySelector('img');
         this.content = this.member.querySelector('.member-cont');
-        console.log(this.content);
+        // console.log(this.img);
 
         this.tab.addEventListener('click', () => this.toggle());
     }
 
     toggle(){
+        this.content.classList.toggle('hide');
+        if(this.img !== null){
+            this.img.classList.toggle('hide');
+        }
         console.log('I was clicked!');
     }
 
